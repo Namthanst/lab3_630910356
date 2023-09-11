81f65d0484bfdfa324606145d4eac740e6be4fa2
Namthanst <139744962+Namthanst@users.noreply.github.com>
2023-09-11 21:01:15 +0700

    Update jsTest.js

--- jsTest.js
+++ jsTest.js
@@ -7,6 +7,7 @@ function checker(){
     const pass = document.querySelector('#psw');
     pass.value;
     console.log(pass.value);
+    if((name.value!=0)&&(pass.value!=0)){
     if((name.value==username)&&(pass.value==password)){
         const title = document.querySelector('p');
         title.textContent='Your username and password are correct! You are logged in.';
@@ -18,6 +19,7 @@ function checker(){
         box.textContent='';
         const title = document.querySelector('p');
         title.textContent='Your username or password is incorrect! Please try again.';
+    }
     }
 }
 const check = document.querySelector('#id1');
